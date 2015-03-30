4eced13a56ec822725d82a2255c7795376e06eb7
Preetham Nadig <preethamnadig@yahoo.com>
2015-03-30 16:00:53 +0800

    enabled cors npm

--- client/www/components/config/config.service.js
+++ client/www/components/config/config.service.js
@@ -2,7 +2,7 @@
 
 angular.module('starter')
 	.factory('Config', function() {
-		var apiBase = window.cordova? "http://fullstack.joshhargreav.es" : "";
+		var apiBase = window.cordova? "http://localhost:8080" : "http://localhost:8080";
 	  return {
 	      apiBase : apiBase
 	  };
--- server/config/express.js
+++ server/config/express.js
@@ -18,6 +18,7 @@ var passport = require('passport');
 var session = require('express-session');
 var mongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 module.exports = function(app) {
   var env = app.get('env');
@@ -30,6 +31,7 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
+  app.use(cors());
   app.use(passport.initialize());
 
   // Persist sessions with mongoStore
